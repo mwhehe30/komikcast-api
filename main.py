157055ba007190d5922128581a6d72dcb3162819
mwhehe30 <zakymubarok05022009@gmail.com>
2026-05-03 08:32:20 +0700

    update origin

--- main.py
+++ main.py
@@ -29,7 +29,12 @@ BASE = "https://be.komikcast.cc"
 
 HEADERS = {
     "accept": "application/json, text/plain, */*",
-    "Referer": "https://v2.komikcast.fit/"
+    "origin": "https://v2.komikcast.fit",
+    "referer": "https://v2.komikcast.fit/",
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36",
+    "sec-ch-ua": '"Google Chrome";v="119", "Chromium";v="119", "Not?A_Brand";v="24"',
+    "sec-ch-ua-mobile": "?0",
+    "sec-ch-ua-platform": '"Windows"',
 }
 
 client = httpx.AsyncClient(
@@ -201,6 +206,7 @@ async def series(
         url = (
             f"{BASE}/series"
             f"?preset=rilisan_terbaru"
+            f"&type=project"
             f"&take={SOURCE_PAGE_SIZE}"
             f"&takeChapter=3"
             f"&page={page}"
